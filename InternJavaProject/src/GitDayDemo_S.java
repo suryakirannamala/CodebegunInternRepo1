f23126e310a15f0b5ab346ef5505dc60e1d03808
Gangam-ShivaSandeep-Reddy <shivasandeepreddygangam@gmail.com>
2024-11-15 23:35:19 +0530

    GitDemo_S
    GitDemo_S

---
+++ InternJavaProject/src/GitDayDemo_S.java
@@ -0,0 +1,21 @@
+package com.test.demo;
+
+public class GitDayDemo_S
+ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello CODEBEGUN");
+
+        System.out.println(" WELCOME TO GIT !");
+
+        System.out.println(" Thanks for the Informative Session!");
+
+         System.out.println(" Successfully Completed!!");
+
+    
+
+	}
+
+}
