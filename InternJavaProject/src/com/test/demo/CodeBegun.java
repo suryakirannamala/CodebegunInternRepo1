4f05f3990b1747f9c3e0b634432d4336fef2eac0
HarishUppula <uppulaharish234@gmail.com>
2024-11-15 20:47:09 +0530

    Update CodeBegun.java

--- InternJavaProject/src/com/test/demo/CodeBegun.java
+++ InternJavaProject/src/com/test/demo/CodeBegun.java
@@ -5,7 +5,7 @@ public class CodeBegun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello CODEBEGUN");
+		System.out.println("Hello");
 
 	}
 
